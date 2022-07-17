2662f2d523e4aefebcf092e94d7f6e52ff10075e
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-17 20:34:12 -0300

    criado função de listar pedidos e sua rota

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { RemoveOrderController } from './controllers/order/RemoveOrderController
 
 import { AddItemController } from './controllers/order/AddItemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
+import { SendOrderController } from './controllers/order/SendOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -50,5 +51,7 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle )
 router.post('/order/add', isAuthenticated, new AddItemController().handle )
 
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle )
 
 export { router }; 
