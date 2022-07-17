ded45c4bf0238dc83d601c25e45c480dfecaeda0
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-17 12:37:12 -0300

    criando a rota product

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,9 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailuserController } from './controllers/user/DetailUserController'
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { CreateProductController } from './controllers/product/CreateProductController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -17,7 +20,12 @@ router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
 
-//-- ROTAS CATEGORY
+//-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+// -- ROTAS PRODUCT --
+router.post('/product', isAuthenticated, new CreateProductController().handle )
 
 export { router }; 
