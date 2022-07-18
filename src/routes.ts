d15e88a8ba7cf8efeb38fe8db797569537c559e2
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-17 21:38:52 -0300

    finish order

--- src/routes.ts
+++ src/routes.ts
@@ -19,6 +19,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 import { ListOrderController } from './controllers/order/ListOrderController'
 import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -59,5 +60,7 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle )
 router.get('/orders', isAuthenticated, new ListOrderController().handle )
 
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
 
 export { router }; 
